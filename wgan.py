d0cb648f16081cbf4acb3295020f79d192bd1023
dslisleedh <gnova6kud@gmail.com>
2022-01-22 20:52:05 +0900

    fix wgan

--- wgan.py
+++ wgan.py
@@ -1,124 +1,115 @@
 import tensorflow as tf
 
 
-class ClipConstraint(tf.keras.constraints.Constraint):
-    def __init__(self, c):
-        self.c = c
-
-    def call(self, weights):
-        return tf.keras.backend.clip(weights, -self.clip_value, self.clip_value)
-
-
-class Wgan(tf.keras.models.Model):
+class Generator(tf.keras.layers.Layer):
     def __init__(self):
-        super(Wgan, self).__init__()
-        self.alpha = .00005
-        self.c = 0.01
-        self.Const = ClipConstraint(self.c)
-        self.n_critic = 5
+        super(Generator, self).__init__()
+        self.ki = tf.keras.initializers.random_normal(stddev=.02)
 
-        self.Generator = tf.keras.Sequential([
-            tf.keras.layers.Dense(4 * 4 * 1024, activation='linear',
-                                  kernel_initializer=tf.keras.initializers.random_normal(stddev=0.01),
-                                  kernel_constraint=self.Const
+        self.upsampling = tf.keras.Sequential([
+            tf.keras.layers.Dense(7 * 7 * 256,
+                                  activation='linear',
+                                  kernel_initializer=self.ki,
+                                  use_bias=False
                                   ),
             tf.keras.layers.BatchNormalization(),
             tf.keras.layers.ReLU(),
-            tf.keras.layers.Reshape((4, 4, 1024)),
-            tf.keras.layers.Conv2DTranspose(filters=512,
-                                            kernel_size=(5, 5),
-                                            strides=(2, 2),
-                                            padding='same',
-                                            activation='linear',
-                                            kernel_initializer=tf.keras.initializers.random_normal(stddev=0.01),
-                                            kernel_constraint=self.Const
-                                            ),
-            tf.keras.layers.BatchNormalization(),
-            tf.keras.layers.ReLU(),
+            tf.keras.layers.Reshape((7, 7, 256)),
             tf.keras.layers.Conv2DTranspose(filters=256,
                                             kernel_size=(5, 5),
                                             strides=(2, 2),
-                                            padding='same',
                                             activation='linear',
-                                            kernel_initializer=tf.keras.initializers.random_normal(stddev=0.01),
-                                            kernel_constraint=self.Const,
-                                            use_bias=False
+                                            padding='same',
+                                            use_bias=False,
+                                            kernel_initializer=self.ki
                                             ),
             tf.keras.layers.BatchNormalization(),
             tf.keras.layers.ReLU(),
             tf.keras.layers.Conv2DTranspose(filters=128,
                                             kernel_size=(5, 5),
                                             strides=(2, 2),
-                                            padding='same',
                                             activation='linear',
-                                            kernel_initializer=tf.keras.initializers.random_normal(stddev=0.01),
-                                            kernel_constraint=self.Const
+                                            padding='same',
+                                            use_bias=False,
+                                            kernel_initializer=self.ki
                                             ),
             tf.keras.layers.BatchNormalization(),
             tf.keras.layers.ReLU(),
             tf.keras.layers.Conv2DTranspose(filters=1,
                                             kernel_size=(5, 5),
-                                            strides=(2, 2),
-                                            padding='same',
                                             activation='tanh',
-                                            kernel_initializer=tf.keras.initializers.random_normal(stddev=0.01),
-                                            kernel_constraint=self.Const
+                                            padding='same',
+                                            kernel_initializer=self.ki
                                             )
         ])
-        self.Generator.build((None, 100))
-        self.Critic = tf.keras.Sequential([
-            tf.keras.layers.Conv2D(filters=64,
-                                   kernel_size=(5, 5),
-                                   padding='same',
-                                   strides=(2, 2),
-                                   activation='linear',
-                                   kernel_initializer=tf.keras.initializers.random_normal(stddev=0.01),
-                                   kernel_constraint=self.Const
-                                   ),
-            tf.keras.layers.BatchNormalization(),
-            tf.keras.layers.LeakyReLU(0.2),
+
+    def call(self, inputs, **kwargs):
+        return self.upsampling(inputs)
+
+
+class Critic(tf.keras.layers.Layer):
+    def __init__(self, c):
+        super(Critic, self).__init__()
+        self.ki = tf.keras.initializers.random_normal(stddev=.02)
+
+        self.downsampling = tf.keras.Sequential([
             tf.keras.layers.Conv2D(filters=128,
                                    kernel_size=(5, 5),
                                    padding='same',
                                    strides=(2, 2),
                                    activation='linear',
-                                   kernel_initializer=tf.keras.initializers.random_normal(stddev=0.01),
-                                   kernel_constraint=self.Const
+                                   kernel_initializer=self.ki
+                                   ),
+            tf.keras.layers.LeakyReLU(.2),
+            tf.keras.layers.Conv2D(filters=128,
+                                   kernel_size=(5, 5),
+                                   padding='same',
+                                   strides=(2, 2),
+                                   activation='linear',
+                                   kernel_initializer=self.ki,
+                                   use_bias=False
                                    ),
             tf.keras.layers.BatchNormalization(),
-            tf.keras.layers.LeakyReLU(0.2),
+            tf.keras.layers.LeakyReLU(.2),
             tf.keras.layers.Conv2D(filters=256,
                                    kernel_size=(5, 5),
                                    padding='same',
                                    strides=(2, 2),
                                    activation='linear',
-                                   kernel_initializer=tf.keras.initializers.random_normal(stddev=0.01),
-                                   kernel_constraint=self.Const
+                                   kernel_initializer=self.ki,
+                                   use_bias=False
                                    ),
             tf.keras.layers.BatchNormalization(),
-            tf.keras.layers.LeakyReLU(0.2),
-            tf.keras.layers.Conv2D(filters=512,
-                                   kernel_size=(5, 5),
-                                   padding='same',
-                                   strides=(2, 2),
-                                   activation='linear',
-                                   kernel_initializer=tf.keras.initializers.random_normal(stddev=0.01),
-                                   kernel_constraint=self.Const
-                                   ),
-            tf.keras.layers.BatchNormalization(),
-            tf.keras.layers.LeakyReLU(0.2),
+            tf.keras.layers.LeakyReLU(.2),
             tf.keras.layers.Conv2D(filters=1,
                                    kernel_size=(4, 4),
-                                   activation='sigmoid',
-                                   kernel_initializer=tf.keras.initializers.random_normal(stddev=0.01),
-                                   kernel_constraint=self.Const,
+                                   activation='linear',
+                                   kernel_initializer=self.ki,
                                    padding='valid'
                                    ),
             tf.keras.layers.Flatten()
         ])
-        self.Critic.build((None, 64, 64, 1))
+
+    def call(self, inputs, **kwargs):
+        return self.downsampling(inputs)
+
+
+class Wgan(tf.keras.models.Model):
+    '''
+    set train batch_size as 64 * n_critic
+    '''
+    def __init__(self):
+        super(Wgan, self).__init__()
+        self.alpha = .00005
+        self.c = 0.01
+        self.n_critic = 5
+
+        self.Generator = Generator()
+        self.Generator.build((None, 100))
+        self.Critic = Critic(self.c)
+        self.Critic.build((None, 28, 28, 1))
         self.compile()
-        self.Resize = tf.keras.layers.experimental.preprocessing.Resizing(height=64, width=64)
+        self.hist = []
 
     def compile(self):
         super(Wgan, self).compile()
@@ -126,47 +117,45 @@ class Wgan(tf.keras.models.Model):
         self.c_optimizer = tf.keras.optimizers.RMSprop(self.alpha)
 
     @tf.function
-    def compute_disc_loss(self, true, fake):
-        loss = tf.reduce_mean(
-            true
-        ) - tf.reduce_mean(
-            fake
-        )
-        return loss
-
-    @tf.function
-    def compute_gen_loss(self, fake):
-        loss = tf.reduce_mean(fake)
-        return loss
-
-    @tf.function
     def train_step(self, img):
-        true_label = -tf.ones((tf.shape(img)[0], 1))
-        fake_label = tf.ones((tf.shape(img)[0], 1))
-        img = self.Resize(img)
+        img = tf.split(img,
+                       axis=0,
+                       num_or_size_splits=self.n_critic
+                       )
 
-        disc_mean_loss = 0
-        for _ in range(self.n_critic):
-            fake_img = self.Generator(tf.random.normal((tf.shape(img)[0], 100)), training=False)
+        # 1. update discriminator
+        critici_mean_loss = 0
+        for i in range(self.n_critic):
             with tf.GradientTape() as tape:
-                true_logit = self.Critic(img, training=True)
-                fake_logit = self.Critic(fake_img, training=True)
-                loss = self.compute_disc_loss(true_logit, fake_logit)
-            grads = tape.gradient(loss, self.Critic.trainable_variables)
+                # Sample x from real data
+                x = img[i]
+                # Sample z from prior
+                z = tf.random.normal(shape=(tf.shape(x)[0], 100))
+                # Compute grads_w : [ 1/m * sigma^m_i=1 f_w(x^i) - 1/m * sigma^m_i=1 f_w(g_theta(z^i)) ]
+                loss = tf.reduce_mean(
+                    self.Critic(x, training=True)
+                ) - tf.reduce_mean(
+                    self.Critic(self.Generator(z, training=False), training=True)
+                )
+            grads_w = tape.gradient(loss, self.Critic.trainable_variables)
+            # Gradient ascent
             self.c_optimizer.apply_gradients(
-                zip(grads, self.Critic.trainable_variables)
+                zip([-g for g in grads_w], self.Critic.trainable_variables)
             )
-            disc_mean_loss += loss
-        disc_mean_loss /= self.n_critic
+            W = self.Critic.trainable_variables
+            [tf.compat.v1.assign(w, tf.clip_by_value(w, -self.c, self.c)) for w in W]
+            critici_mean_loss += loss
 
+        # 2. update generator
         with tf.GradientTape() as tape:
-            fake_img = self.Generator(tf.random.normal((tf.shape(img)[0], 100)), training=True)
-            fake_logit = self.Critic(fake_img, training=False)
-            gen_loss = self.compute_gen_loss(fake_logit)
-        grads = tape.gradient(gen_loss, self.Generator.trainable_variables)
+            # Sample z from prior
+            z = tf.random.normal(shape=(tf.shape(x)[0], 100))
+            # Compute grads_theta : - 1/m * sigma^m_i=1 f_w(g_theta(z^i))
+            loss = -tf.reduce_mean(self.Critic(self.Generator(z, training=True), training=False))
+        grads_theta = tape.gradient(loss, self.Generator.trainable_variables)
+        # Gradient descent
         self.g_optimizer.apply_gradients(
-            zip(grads, self.Generator.trainable_variables)
+            zip(grads_theta, self.Generator.trainable_variables)
         )
 
-        return {'mean_discrimination_loss': disc_mean_loss, 'generation_loss': gen_loss}
-
+        return {'mean_criticization_loss': critici_mean_loss / self.n_critic, 'generation_loss': loss}
